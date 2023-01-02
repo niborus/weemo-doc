a9475aa2c053fb6e6f8896efb217eb38f3713ddd
Inkception <66379522+Inkception@users.noreply.github.com>
2023-01-02 07:39:14 +0100

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Weemo'
-copyright = u'2021 - 2022, Weemo-Team'
+copyright = u'2021 - 2023, Weemo-Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
